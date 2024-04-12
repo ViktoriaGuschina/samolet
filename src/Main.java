11d650178e9bb824c84583e57482f8bb7e75aa09
VIKS NEW <guschina.viktoria@gmail.com>
2024-04-12 18:15:32 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100_000;
+        int oneMile = 20;
+
+        int bonus = amount / oneMile;
+        System.out.println(bonus);
+    }
+
+}
